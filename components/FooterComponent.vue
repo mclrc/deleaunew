65b7e3cac8d389285e6aaf562a023650c038b563
Moritz <moritzamando@proton.me>
2023-02-04 16:51:59 +0100

    Fix content, update footer

--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -1,72 +1,75 @@
 <template>
-	<footer>
-		<hr />
-		<div id="uls">
-			<div>
-				<ul>
-					<li>Roland Deleau</li>
-					<li>Muskauer Strasse 20a<br>10997 Berlin</li>
-					<li>Mail: <a href="mailto:roland@deleau.de">roland@deleau.de</a></li>
-					<li>Phone: +49 1575 3535828</li>
-				</ul>
-			</div>
-			<div>
-				<ul>
-					<li><a href="https://www.galerie-schmalfuss.de/">Galerie Schmalfuss</a></li>
-					<li><a href="http://www.thorens-gallery.com/">Thorens Gallery</a></li>
-					<li>Website by <a href="https://moritzclc.me">Moritz Clerc</a></li>
-				</ul>
-			</div>
-		</div>
-		<p id="copyright">©{{new Date().getFullYear()}} Roland Deleau</p>
-	</footer>
+  <footer>
+    <hr />
+    <div id="uls">
+      <div>
+        <ul>
+          <li>Roland Deleau</li>
+          <li>15, Place du Marché</li>
+          <li>F-26170 Buis Les Baronnies</li>
+          <li>+33 638 59 43 69</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href="https://deleau-buis.com">Deleau-Buis Ferienwohnung</a>
+          </li>
+          <li>
+            Seite entwickelt von <a href="https://moritzclc.me">Moritz</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <p id="copyright">©{{ new Date().getFullYear() }} Roland Deleau</p>
+  </footer>
 </template>
 
 <style scoped lang="scss">
-@import '@/assets/style/vars.scss';
+@import "@/assets/style/vars.scss";
 
 footer {
-	font-size: .9em;
-	font-family: Quicksand;
-	max-width: $max-content-width;
-	padding: 2rem $content-padding;
-	display: block;
-	margin: 0 auto;
+  font-size: 0.9em;
+  font-family: Quicksand;
+  max-width: $max-content-width;
+  padding: 2rem $content-padding;
+  display: block;
+  margin: 0 auto;
 }
 hr {
-	margin-bottom: 2rem;
+  margin-bottom: 2rem;
 }
 ul {
-	margin: 2 auto;
-	padding: 0;
-	list-style: none;
-	color: $text-color;
-	a {
-		color: inherit;
-		text-decoration: none;
-		font-weight: bold;
-	}
+  margin: 2 auto;
+  padding: 0;
+  list-style: none;
+  color: $text-color;
+  a {
+    color: inherit;
+    text-decoration: none;
+    font-weight: bold;
+  }
 }
 #copyright {
-	text-align: center;
-	margin: 1rem 0;
+  text-align: center;
+  margin: 1rem 0;
 }
 
 @media screen and (min-width: $breakpoint) {
-	#uls {
-		display: flex;
-		> * {
-			flex: 1;
-			display: flex;
-			justify-content: center;
-		}
-		ul {
-			display: inline-block;
-			margin: 0 auto;
-		}
-		*:not(:last-child) {
-			margin-right: 1rem;
-		}
-	}
+  #uls {
+    display: flex;
+    > * {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+    }
+    ul {
+      display: inline-block;
+      margin: 0 auto;
+    }
+    *:not(:last-child) {
+      margin-right: 1rem;
+    }
+  }
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,17 @@
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
-	app: {
-		head: {
-			script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
-			meta: [
-				{ name: 'description', content: 'Persönliche Seite von Roland Deleau' },
-			]
-		},
-	},
-	modules: [
-		'@nuxt/content',
-	],
-	content: {
-		documentDriven: true,
-	},
-})
+  app: {
+    head: {
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+      ],
+      meta: [
+        { name: "description", content: "Persönliche Seite von Roland Deleau" },
+      ],
+    },
+  },
+  modules: ["@nuxt/content"],
+  buildModules: ["@nuxt/typescript-build"],
+  content: {
+    documentDriven: true,
+  },
+});
